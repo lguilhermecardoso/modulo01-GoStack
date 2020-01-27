64db37c4657a1a48d95f16395b7b344e275d84c6
Guilherme Cardoso <guilhermecardoso.info@gmail.com>
2020-01-27 17:41:49 -0300

    CRUD simples com NODE sem Banco

--- index.js
+++ index.js
@@ -2,7 +2,45 @@ const express = require('express');
 
 const server = express();
 
+server.use(express.json())
+
 const users = ['Diego', 'Claudio', 'Victor']
+
+server.get('/users', (req, res) => {
+  return res.json(users);
+})
+
+server.post('/users', (req, res) => {
+  
+  const { name } = req.body
+  
+  users.push(name);
+  
+  return res.json(users);
+  
+})
+
+server.delete('/users/:index', (req, res) => {
+
+  const { index } = req.params;
+
+  users.splice(index, 1);
+
+  return res.json(users);
+  
+})
+
+server.put('/users/:index', (req, res) => {
+
+  const { index } = req.params;
+
+  const { name } = req.body;
+
+  users[index] = name;
+
+  return res.json(users);
+
+});
 
 server.get('/users/:index',  (req, res) => {
 
