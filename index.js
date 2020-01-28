b561c28c01505821b117d214f1543a09696ee811
Guilherme Cardoso <guilhermecardoso.info@gmail.com>
2020-01-28 12:39:54 -0300

    Aula de Middlewares

--- index.js
+++ index.js
@@ -6,11 +6,32 @@ server.use(express.json())
 
 const users = ['Diego', 'Claudio', 'Victor']
 
+//Middleware GLOBAL
+server.use((req, res, next) => {
+
+  console.time('Request')
+
+  console.log(`Método: ${req.method}: URL: ${req.url}`);
+
+  next();
+
+  console.timeEnd('Request')
+
+});
+
 server.get('/users', (req, res) => {
   return res.json(users);
 })
+// Middleware Local
+function checkUsersExists(req, res, next) {
+  if (!req.body.name) {
+    return res.status(400).json({error: 'User name is required'})
+  }
 
-server.post('/users', (req, res) => {
+  return next();
+}
+
+server.post('/users', checkUsersExists, (req, res) => {
   
   const { name } = req.body
   
@@ -26,11 +47,11 @@ server.delete('/users/:index', (req, res) => {
 
   users.splice(index, 1);
 
-  return res.json(users);
+  return res.send();
   
 })
 
-server.put('/users/:index', (req, res) => {
+server.put('/users/:index', checkUsersExists, (req, res) => {
 
   const { index } = req.params;
 
